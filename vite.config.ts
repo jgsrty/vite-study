846cb60d0aa7999c81a893d51f1721393efd903b
RtyXmd <rongtianyang@inin88.com>
2022-08-02 15:46:36 +0800

    add sass

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
-import { defineConfig } from "vite";
+import { defineConfig, normalizePath } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+
+const variablePath = normalizePath(path.resolve("./src/variable.scss"));
 
 // https://vitejs.dev/config/
 export default defineConfig({
   root: path.join(__dirname, "src"),
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "${variablePath}";`,
+      },
+    },
+  },
 });
