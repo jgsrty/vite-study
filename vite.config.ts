94d6d287f14125f38cd56725e30e7c2f2373a15d
RtyXmd <rongtianyang@inin88.com>
2022-08-02 15:55:54 +0800

    'autoprefixer'

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, normalizePath } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import autoprefixer from "autoprefixer";
 
 const variablePath = normalizePath(path.resolve("./src/variable.scss"));
 
@@ -16,6 +17,9 @@ export default defineConfig({
       scss: {
         additionalData: `@import "${variablePath}";`,
       },
+    },
+    postcss: {
+      plugins: [autoprefixer({})],
     },
   },
 });
